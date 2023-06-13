61ffa9ae2d91d474e33eec1f070c57a5125f2842
dazino <kkytwo90@gmail.com>
2023-06-14 00:42:31 +0900

    두번째 커밋

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native'
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>챌린지1기 앱 만들기</Text>
+      <Text>챌린지1기 앱 만들기2213123</Text>
       <StatusBar style="auto" />
     </View>
   )
